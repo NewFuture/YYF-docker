ae16d0f6e08d6ec967ffe0697353dc840daa552f
NewFuture <github@newfuture.cc>
2017-01-13 20:42:59 +0800

    自动初始化数据库

--- run.sh
+++ run.sh
@@ -5,30 +5,37 @@ set -e
 WORK_DIR=${WORK_DIR:-'/yyf'}
 MYSQL_DIR=${MYSQL_DIR:-"$WORK_DIR/runtime/mysql"}
 MYSQL_USER=${MYSQL_USER:-'root'}
-MYSQL_PASSWORD=${MYSQL_PASSWORD:-''}
 WEB_ROOT=${WEB_ROOT:-"$WORK_DIR/public"}
 MYSQL_SCRIPT=${MYSQL_SCRIPT:-"$WORK_DIR/tests/mysql.sql"}
-tempSqlFile='/tmp/mysql-first-time.sql'
+
+tempSqlFile='/tmp/mysql-init.sql'
 
 # start redis
 redis-server &
 memcached -u memcached &
 
-#start mysql
+#init and start mysql
 [ -d "/run/mysqld" ] || mkdir -p /run/mysqld;
-[ -d "$MYSQL_DIR" ] || mkdir -p $MYSQL_DIR;
-chown -R mysql:mysql "$WORK_DIR" /run/mysqld
-test -f "$MYSQL_DIR/mysql-bin.index" || mysql_install_db --user=mysql --datadir="$MYSQL_DIR"
-echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';">"$tempSqlFile";
-echo "GRANT ALL ON *.* TO '$MYSQL_USER'@'%';">>"$tempSqlFile";
-echo 'FLUSH PRIVILEGES ;' >> "$tempSqlFile";
-test -f $MYSQL_SCRIPT && echo "source $MYSQL_SCRIPT ;">>"$tempSqlFile";
-if [ -f '$WORK_DIR/tests/yyf.sql' ];then
-    sed '/^\/\*MYSQL/d;/MYSQL\*\//d' tests/yyf.sql >> "$tempSqlFile";
-    # sed '/^\/\*SQLITE/d;/SQLITE\*\//d' tests/yyf.sql | sqlite3 runtime/yyf.db;
+chown -R mysql:mysql "$WORK_DIR" /run/mysqld;
+if [ -f "$MYSQL_DIR/mysql-bin.index" ];then
+    mysqld -u mysql --datadir "$MYSQL_DIR" &
+else
+    [ -d "$MYSQL_DIR" ] || mkdir -p $MYSQL_DIR;
+    mysql_install_db --user=mysql --skip-name-resolve --datadir="$MYSQL_DIR";
+    echo -e "DELETE FROM mysql.user;\nFLUSH PRIVILEGES;">"$tempSqlFile";
+    if [ $MYSQL_PASSWORD ];then
+        echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';">>"$tempSqlFile";
+    else
+        mysqld_safe --skip-grant-tables --skip-networking &
+        echo "CREATE USER '$MYSQL_USER'@'%';">>"$tempSqlFile";
+    fi;
+    echo -e "GRANT ALL ON *.* TO '$MYSQL_USER'@'%';\nFLUSH PRIVILEGES;">>"$tempSqlFile";
+    [ -f "$WORK_DIR/tests/yyf.sql" ]&& sed '/^\/\*MYSQL/d;/MYSQL\*\//d' "$WORK_DIR/tests/yyf.sql" >> "$tempSqlFile";
+    [ -f $MYSQL_SCRIPT ]&& cat "$MYSQL_SCRIPT">>"$tempSqlFile";
+    mysqld -u mysql --datadir "$MYSQL_DIR" --init-file="$tempSqlFile" &
 fi;
-mysqld_safe --skip-grant-tables --skip-networking &
-mysqld -u mysql --datadir "$MYSQL_DIR" --init-file="$tempSqlFile" &
+
+# sed '/^\/\*SQLITE/d;/SQLITE\*\//d' tests/yyf.sql | sqlite3 runtime/yyf.db;
 
 # run init script in tests
 for file in tests/init.* ;do
