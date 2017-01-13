0a3b90aa399e684efaad61995a14ddc4f5c507b9
NewFuture <github@newfuture.cc>
2017-01-13 16:23:06 +0800

    init sql

--- run.sh
+++ run.sh
@@ -2,11 +2,12 @@
 
 set -e
 
-WORK_DIR=${WORK_DIR:-'/yyf/'}
+WORK_DIR=${WORK_DIR:-'/yyf'}
 MYSQL_DIR=${MYSQL_DIR:-"$WORK_DIR/runtime/mysql"}
 MYSQL_USER=${MYSQL_USER:-'root'}
 MYSQL_PASSWORD=${MYSQL_PASSWORD:-''}
 WEB_ROOT=${WEB_ROOT:-"$WORK_DIR/public"}
+MYSQL_SCRIPT=${MYSQL_SCRIPT:-"$WORK_DIR/tests/mysql.sql"}
 tempSqlFile='/tmp/mysql-first-time.sql'
 
 # start redis
@@ -17,10 +18,15 @@ memcached -u memcached &
 [ -d "/run/mysqld" ] || mkdir -p /run/mysqld;
 [ -d "$MYSQL_DIR" ] || mkdir -p $MYSQL_DIR;
 chown -R mysql:mysql "$WORK_DIR" /run/mysqld
-echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';">"$tempSqlFile"
-echo "GRANT ALL ON *.* TO '$MYSQL_USER'@'%';">>"$tempSqlFile"
+test -f "$MYSQL_DIR/mysql-bin.index" || mysql_install_db --user=mysql --datadir="$MYSQL_DIR"
+echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';">"$tempSqlFile";
+echo "GRANT ALL ON *.* TO '$MYSQL_USER'@'%';">>"$tempSqlFile";
 echo 'FLUSH PRIVILEGES ;' >> "$tempSqlFile";
-mysql_install_db --user=mysql --datadir="$MYSQL_DIR"
+test -f $MYSQL_SCRIPT && echo "source $MYSQL_SCRIPT ;">>"$tempSqlFile";
+if [ -f '$WORK_DIR/tests/yyf.sql' ];then
+    sed '/^\/\*MYSQL/d;/MYSQL\*\//d' tests/yyf.sql >> "$tempSqlFile";
+    # sed '/^\/\*SQLITE/d;/SQLITE\*\//d' tests/yyf.sql | sqlite3 runtime/yyf.db;
+fi;
 mysqld -u mysql --datadir "$MYSQL_DIR" --init-file="$tempSqlFile" &
 
 # run init script in tests
@@ -39,16 +45,7 @@ for file in tests/*.sh; do
     fi
 done;
 
-# init database
-# if [ -f 'tests/yyf.sql' ];then
-#     sed '/^\/\*MYSQL/d;/MYSQL\*\//d' tests/yyf.sql | mysql -uroot;
-#     sed '/^\/\*SQLITE/d;/SQLITE\*\//d' tests/yyf.sql | sqlite3 runtime/yyf.db;
-# fi;
-# if [ -f 'tests/mysql.sql' ]; then
-#     mysql -uroot mysql < tests/mysql.sql;
-# fi;
-
 # run
-php -S 0.0.0.0:$PORT  $([ -d public ]&&echo '-t public') &
+php -S 0.0.0.0:$PORT  $([ -d $WEB_ROOT ]&&echo "-t $WEB_ROOT") &
 
 # exec "$@"
