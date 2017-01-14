5e76dce2332417d0fcd13e9f50a48a1682904a7c
NewFuture <github@newfuture.cc>
2017-01-14 22:12:39 +0800

    full shell

--- Dockerfile
+++ Dockerfile
@@ -1,31 +1,31 @@
 FROM newfuture/yaf:latest
 MAINTAINER New Future <docker@newfuture.cc>
-
 LABEL Name="YYF-docker" Description="mimimal docker image for YYF"
 
 # Environments
 ENV PORT=80 \
 	TIMEZONE=UTC \
 	MAX_UPLOAD=50M \
-	WORK_DIR=/yyf
+	WORK_DIR=/yyf/
 
-# instal PHP
-RUN apk add --no-cache \
-	# install redis and mysql memcached
-		redis \
-		mariadb \
-		memcached \
+# instal
+RUN apk add --update
+RUN apk add --no-cache redis memcached
+RUN apk add --no-cache sqlite
+RUN apk add --no-cache mariadb
+
 	# ClEAN
-	&& rm -rf /var/cache/apk/* \
-		/var/tmp/* \
-		/tmp/* \
-		/etc/ssl/certs/*.pem \
-		/etc/ssl/certs/*.0 \
-		/usr/share/ca-certificates/mozilla/* \
-		/usr/share/man/* \
-		/usr/include/*
+RUN rm -rf /var/cache/apk/* \
+	/var/tmp/* \
+	/tmp/* \
+	/etc/ssl/certs/*.pem \
+	/etc/ssl/certs/*.0 \
+	/usr/share/ca-certificates/mozilla/* \
+	/usr/share/man/* \
+	/usr/include/*
 
 WORKDIR $WORK_DIR
+
 
 COPY ./run.sh /run.sh
 
--- full/run.sh
+++ full/run.sh
@@ -2,11 +2,18 @@
 
 set -e
 
+#项目目录
 WORK_DIR=${WORK_DIR:-'/yyf/'}
+#数据库目录
 MYSQL_DIR=${MYSQL_DIR:-"${WORK_DIR}runtime/mysql"}
-MYSQL_USER=${MYSQL_USER:-'root'}
-WEB_ROOT=${WEB_ROOT:-"${WORK_DIR}public"}
+#MYSQL 账号
+MYSQL_ACCOUNT=${MYSQL_ACCOUNT:-'root'}
+#MYSQL_初始化脚本
 MYSQL_SCRIPT=${MYSQL_SCRIPT:-"${WORK_DIR}tests/mysql.sql"}
+#SQLite数据库存放位置
+SQLITE_FILE=${SQLITE_FILE:-"${WORK_DIR}runtime/yyf.db"}
+WEB_ROOT=${WEB_ROOT:-"${WORK_DIR}public"}
+
 
 tempSqlFile='/tmp/mysql-init.sql'
 
@@ -24,18 +31,34 @@ else
     mysql_install_db --user=mysql --skip-name-resolve --datadir="$MYSQL_DIR";
     echo -e "DELETE FROM mysql.user;\nFLUSH PRIVILEGES;">"$tempSqlFile";
     if [ $MYSQL_PASSWORD ];then
-        echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';">>"$tempSqlFile";
+        #MYSQL 账号
+        echo "CREATE USER '$MYSQL_ACCOUNT'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';">>"$tempSqlFile";
+    #MYSQL_初始化脚本
     else
+        #SQLite数据库存放位置
         mysqld_safe --skip-grant-tables --skip-networking &
-        echo "CREATE USER '$MYSQL_USER'@'%';">>"$tempSqlFile";
+        #MYSQL 账号
+        echo "CREATE USER '$MYSQL_ACCOUNT'@'%';">>"$tempSqlFile";
+    #MYSQL_初始化脚本
     fi;
-    echo -e "GRANT ALL ON *.* TO '$MYSQL_USER'@'%';\nFLUSH PRIVILEGES;">>"$tempSqlFile";
+    #SQLite数据库存放位置
+    #MYSQL 账号
+    echo -e "GRANT ALL ON *.* TO '$MYSQL_ACCOUNT'@'%';\nFLUSH PRIVILEGES;">>"$tempSqlFile";
+    #MYSQL_初始化脚本
     [ -f "${WORK_DIR}tests/yyf.sql" ]&& sed '/^\/\*MYSQL/d;/MYSQL\*\//d' "${WORK_DIR}tests/yyf.sql" >> "$tempSqlFile";
+    #SQLite数据库存放位置
     [ -f $MYSQL_SCRIPT ]&& cat "$MYSQL_SCRIPT">>"$tempSqlFile";
     mysqld -u mysql --datadir "$MYSQL_DIR" --init-file="$tempSqlFile" &
 fi;
 
-# sed '/^\/\*SQLITE/d;/SQLITE\*\//d' tests/yyf.sql | sqlite3 runtime/yyf.db;
+# 初始化SQLite数据库
+if [ -f "$SQLITE_FILE" ];then #文件不存在在自动初始化
+    if [ $SQLITE_SCRIPT ]; then
+        cat "$SQLITE_SCRIPT" | sqlite3 $SQLITE_FILE;
+    elif [ -f "${WORK_DIR}tests/yyf.sql" ]; then
+        sed '/^\/\*SQLITE/d;/SQLITE\*\//d' "${WORK_DIR}tests/yyf.sql" | sqlite3 $SQLITE_FILE;
+    fi
+fi;
 
 # run init script in tests
 for file in tests/init.* ;do
